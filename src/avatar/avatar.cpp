e95de561a1c8de20d8fca50c62195889d9b1d9b2
Hpsaturn <hpsaturn@gmail.com>
2026-04-09 00:01:04 +0200

    testing with external image

---
+++ src/avatar/avatar.cpp
@@ -0,0 +1,58 @@
+/* Simple firmware for a ESP32 displaying a static image on an EPaper Screen.
+ *
+ * Write an image into a header file using a 3...2...1...0 format per pixel,
+ * for 4 bits color (16 colors - well, greys.) MSB first.  At 80 MHz, screen
+ * clears execute in 1.075 seconds and images are drawn in 1.531 seconds.
+ */
+
+#include <Arduino.h>
+#include "epd_driver.h"
+#include "image.h"
+
+uint8_t *framebuffer;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    epd_init();
+
+    framebuffer = (uint8_t *)heap_caps_malloc(EPD_WIDTH * EPD_HEIGHT / 2, MALLOC_CAP_SPIRAM);
+    memset(framebuffer, 0xFF, EPD_WIDTH * EPD_HEIGHT / 2);
+
+}
+
+void update(uint32_t delay_ms)
+{
+    epd_poweron();
+    epd_clear();
+    volatile uint32_t t1 = millis();
+    epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    volatile uint32_t t2 = millis();
+    Serial.printf("EPD draw took %dms.\n", t2 - t1);
+    epd_poweroff();
+    delay(delay_ms);
+}
+
+void loop()
+{
+    Rect_t area = {
+        .x = 0,
+        .y = 0,
+        .width = avatar_width,
+        .height =  avatar_height
+    };
+    epd_poweron();
+    epd_clear();
+    epd_draw_grayscale_image(area, (uint8_t *) avatar_data);
+    epd_poweroff();
+    delay(3000);
+
+    epd_poweroff();
+    delay(1000);
+    
+    epd_poweroff_all();
+
+    esp_sleep_enable_ext1_wakeup(GPIO_SEL_39, ESP_EXT1_WAKEUP_ALL_LOW);
+    esp_deep_sleep_start();
+}
